b6e678d8df72ddc9bbf5177ee7e2ccc4edebd484
Aleksandr Pavlov <pavlovaleksandr@ukr.net>
2022-06-19 22:08:41 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,90 +1,91 @@
-import { fetchImages } from "./js/get_pixabay.js";
+import ImgApiService from "./js/get_pixabay.js";
 import Notiflix from 'notiflix';
 
 
 const refs = {
     searchForm: document.querySelector('#search-form'),
     searchInput: document.querySelector('input'),
-    searchBtn: document.querySelector('button'),
+    searchBtn: document.querySelector('.search'),
+    loadBtn: document.querySelector('.load-more'),
     gallaryList: document.querySelector('.gallery')
+
 }
 console.log(refs);
-console.log(fetchImages('cat', 1, 10));
+
+const imgApiService = new ImgApiService();
+// console.log(fetchImages('cat'));
 
 
-function emptyArray() {
-    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-}
+refs.searchForm.addEventListener('submit', onSearchSubmit)
+refs.loadBtn.addEventListener('click', onLoadMore)
 
-refs.searchForm.addEventListener('click', toGetPixabay);
 
-function toGetPixabay(e) {
+
+function onSearchSubmit(e) {
     e.preventDefault();
-    const name = refs.searchInput.value;
-    if (name === '') {
-       return emptyArray();
-    }
-}
+    imgApiService.query = refs.searchInput.value;
+    imgApiService.resetPage();
+    imgApiService.fetchImages(imgApiService.query, imgApiService.page, imgApiService.perPage).then(appendImgMarkup);
 
-fetchImages(query).then(data => {
-    refs.gallaryList.innerHTML = "";
-    if (data.length === 1) {
-        refs.gallaryList.insertAdjacentHTML(`beforeend`, renderGallery(data));
-    }
-}
-).catch(error => { console.log(error); }
-);
-
-
-function renderGallery(images) {
-    const markup = dataImg.map(({largeImageURL,tags,likes,view,comments,downloads }) => {
-        return `
-<div class="photo-card">
-        <img src="${largeImageURL}" alt="${tags}" loading="lazy" />
-    <div class="info">
-        <p class="info-item">
-         <b>Likes: ${likes}</b>
-        </p>
-        <p class="info-item">
-        <b>Views: ${view}</b>
-        </p>
-        <p class="info-item">
-         <b>Comments: ${comments}</b>
-        </p>
-        <p class="info-item">
-         <b>Downloads: ${downloads}</b>
-        </p>
-  </div>
-</div>
-        `
-
-    }).join('');
-    return markup;
     
 }
 
 
+function onLoadMore() {
+    imgApiService.fetchImages(imgApiService.query, imgApiService.page, imgApiService.perPage).then(appendImgMarkup);   
+}
 
+function appendImgMarkup(images) {
+    
+    const markup = images.map(({ webformatURL, tags, likes, comments, views, downloads }) => {
+        return `
+   <div class="photo-card">
+  <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${downloads}</b>
+    </p>
+  </div>
+</div>
+    `;
+    }).join('');
+    return refs.gallaryList.insertAdjacentHTML('beforeend', markup);
 
-// refs.searchForm.addEventListener('submit', e => {
-//     e.preventDefault();
-//     const query = refs.searchInput.value;
-//     if (query === '') {
-//         emptyArray();
-//     } else {
-//         getPixabay(query)
-//             .then(data => {
-//                 if (data.total === 0) {
-//                     emptyArray();
-//                 } else {
-//                     renderGallery(data.hits);
-//                 }
-//             })
-//             .catch(error => {
-//                 console.log(error);
-//             }
-//         );
-//     }
+}
+
+//     const markup = `
+//     <div class="photo-card">
+//   <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+//   <div class="info">
+//     <p class="info-item">
+//       <b>Likes: ${likes}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Views:${views}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Comments: ${comments}</b>
+//     </p>
+//     <p class="info-item">
+//       <b>Downloads;${downloads}</b>
+//     </p>
+//   </div>
+// </div>
+// `
+//     refs.gallaryList.insertAdjacentHTML('beforeend', markup);
 // }
-// );
+
+
+
+
+
 
--- src/js/get_pixabay.js
+++ src/js/get_pixabay.js
@@ -1,12 +1,47 @@
 import axios from 'axios'
-export { fetchImages };
+// export { fetchImages };
 
-axios.defaults.baseURL = 'https://pixabay.com/api/'
-const KEY = '28107487-df53024f81d649718d09de179'
+// axios.defaults.baseURL = 'https://pixabay.com/api/'
+// const KEY = '28107487-df53024f81d649718d09de179'
 
-async function fetchImages(query, page, perPage) {
-  const response = await axios.get(
-    `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
-  )
-  return response
+// async function fetchImages(query, page, perPage) {
+//   const response = await axios.get(
+//     `?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+//   )
+//   return response
+// }
+
+
+
+export default class ImgApiService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.perPage = 5;
+    this.KEY = '28107487-df53024f81d649718d09de179';
+    axios.defaults.baseURL = 'https://pixabay.com/api/';
+  }
+  async fetchImages(query, page, perPage) {
+    const response = await axios.get(
+      `?key=${this.KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`,
+    )
+    this.incrementPage(); 
+    return response.data.hits;
+  }
+
+  incrementPage() {
+    this.page += 1;
+  }
+  resetPage() {
+    this.page = 1;
+  }
+
+
+  get query () {
+    return this.searchQuery;
+  }
+  set query (newQuery) {
+    this.searchQuery = newQuery;
+  }
+
 }
